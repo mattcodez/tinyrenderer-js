3a831e9d0a03dbcbba8e8929c98ba7a4ea0d6e04
Matthew Molnar <matthew.molnar@gmail.com>
2016-03-19 23:13:22 -0400

    Add set method

--- CanvasImage.js
+++ CanvasImage.js
@@ -1,7 +1,17 @@
 class CanvasImage {
-  constructor(canvas){
+  constructor(canvas, width, height){
     //canvas -> actual canvas DOM object
+    //width  -> pixel width of image
+    //height -> pixel height of image
     this.canvas = canvas;
     this.context = canvas.getContext('2d');
+    this.width = width;
+    this.height = height;
+    
+    this.data = new ArrayBuffer(x * y);
+  }
+  
+  set(x, y, color){
+    this.data[(y * this.width) + x] = color;
   }
 }

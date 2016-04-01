31bf0847f4510cd1adff6d86d4211d1b975f33a2
Matthew Molnar <matthew.molnar@gmail.com>
2016-03-31 21:33:44 -0400

    Can now run code from editor. TODO: clear canvas before each run

--- lesson_code/L1E1/main.js
+++ lesson_code/L1E1/main.js
@@ -4,7 +4,7 @@ const white = [255, 255, 255, 255];
 const red   = [255, 0,   0,   255];
 
 function line(x0, y0, x1, y1, image, color) {
-  for (let t=0; t<1; t+=0.01) {
+  for (let t=0; t < 1; t+=0.01) {
     const x = parseInt(x0*(1-t) + x1*t, 10);
     const y = parseInt(y0*(1-t) + y1*t, 10);
     image.set(x, y, color);
@@ -17,3 +17,5 @@ function init(canvasEl) {
   image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
   image.render();
 }
+
+init //For evaling

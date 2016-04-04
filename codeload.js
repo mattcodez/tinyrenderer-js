088312cce998e1cf24270b78bd7aaead5cd6a8c0
Matthew Molnar <matthew.molnar@gmail.com>
2016-04-03 23:40:57 -0400

    More modularizing

--- codeload.js
+++ codeload.js
@@ -1,4 +1,10 @@
 "use strict";
+
+document.getElementById('runCode').addEventListener('click', runCode);
+document.getElementById('chooseCode').addEventListener('click', e => {
+  //TODO: get selected value to build path for retrieveCode()
+});
+
 let myCodeMirror;
 function runCode(e){
   try{
@@ -10,16 +16,17 @@ function runCode(e){
   }
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  fetch('main.js')
-  .then(r => r.text())
-  .then(mainjs => {
-    myCodeMirror = CodeMirror(document.body, {
-      value: mainjs,
-      mode:  "javascript"
-    });
+function retrieveCode(path){
+  document.addEventListener("DOMContentLoaded", function(event) {
+    fetch(path)
+    .then(r => r.text())
+    .then(mainjs => {
+      myCodeMirror = CodeMirror(document.body, {
+        value: mainjs,
+        mode:  "javascript"
+      });
 
-    runCode(mainjs);
-    document.getElementById('runCode').addEventListener('click', runCode);
+      runCode(mainjs);
+    });
   });
-});
+}

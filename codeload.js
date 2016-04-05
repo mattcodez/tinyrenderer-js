4c73edf1aa86946413f04d1902c407a6f63068a4
Matthew Molnar <matthew.molnar@gmail.com>
2016-04-04 23:43:38 -0400

    Dynamic lesson loading partially working.

--- codeload.js
+++ codeload.js
@@ -1,8 +1,10 @@
 "use strict";
 
 document.getElementById('runCode').addEventListener('click', runCode);
-document.getElementById('chooseCode').addEventListener('click', e => {
-  //TODO: get selected value to build path for retrieveCode()
+document.getElementById('chooseCode').addEventListener('change', e => {
+  const select = e.target;
+  const opt = select.options[select.selectedIndex];
+  retrieveCode(`lesson_code/${opt.value}/main.js`);
 });
 
 let myCodeMirror;
@@ -17,16 +19,15 @@ function runCode(e){
 }
 
 function retrieveCode(path){
-  document.addEventListener("DOMContentLoaded", function(event) {
-    fetch(path)
-    .then(r => r.text())
-    .then(mainjs => {
-      myCodeMirror = CodeMirror(document.body, {
-        value: mainjs,
-        mode:  "javascript"
-      });
-
-      runCode(mainjs);
+  fetch(path)
+  .then(r => r.text())
+  .then(mainjs => {
+    //TODO: only create one CodeMirror instance 
+    myCodeMirror = CodeMirror(document.body, {
+      value: mainjs,
+      mode:  "javascript"
     });
+
+    runCode(mainjs);
   });
 }
